03b0fe376204265be65e3e9262356712964e7b7a
DiegoV1ll4 <94655830+DiegoV1ll4@users.noreply.github.com>
2022-04-17 17:15:07 -0300

    Add files via upload

---
+++ Clase3 -Villalba/js/miarchivo.js
@@ -0,0 +1,17 @@
+//ingresar a una pagina Nombre y numero
+
+
+let entrada = prompt("ingrese su nombre");
+    alert ("Hola " + entrada);
+
+while (entrada) {
+entrada= parseFloat(prompt("ingrese un numero"))
+let condicion = prompt ("Salir?").toLowerCase()
+
+if (condicion == "si"){
+numero = numeroIngresado
+}
+}
+
+
+
